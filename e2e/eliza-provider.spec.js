c2a70f16bd108d776452c5beebc4e38fd8a14729
jennerdulce <jennerdulce@gmail.com>
2025-10-27 18:42:58 -0700

    test: skeleton for eliza tests

---
+++ e2e/eliza-provider.spec.js
@@ -0,0 +1,13 @@
+// @ts-check
+import { test, expect } from '@playwright/test';
+
+test.describe('Eliza AI Provider Tests', () => {
+  test.beforeEach(async ({ page }) => {
+  
+  });
+
+  test('should respond to basic user message', async ({ page }) => {
+    
+  });
+
+});
